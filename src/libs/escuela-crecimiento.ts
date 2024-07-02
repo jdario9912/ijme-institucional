6b2545570aae245c778374ffdcdc079e1631829c
Joel <joeldario9912@gmail.com>
2024-07-01 22:41:07 -0300

    escuela crecimiento

---
+++ src/libs/escuela-crecimiento.ts
@@ -0,0 +1,41 @@
+type Materia = {
+  titulo: string;
+  parrafos: string[];
+};
+
+export const materias: Materia[] = [
+  {
+    titulo: "Primeros Pasos",
+    parrafos: [
+      "Respondemos a preguntas como: ¿Qué significa ser cristiano? o ¿Qué es el evangelismo?",
+      "Obtendrás un panorama de tu nuevo propósito, fe y estilo de vida. También recibirás una introducción a los símbolos Biblicos de nuestra salvación: el Bautismo y la Santa Cena.",
+    ],
+  },
+  {
+    titulo: "Comunidad",
+    parrafos: [
+      "¿Alguna vez te preguntaste qué significa ser miembro de una iglesia? o ¿Porqué nos congregamos?",
+      "Éste curso es la introducción básica de lo que necesitas saber sobre la familia de la fe.",
+    ],
+  },
+  {
+    titulo: "Madurez",
+    parrafos: [
+      "Dios desea que cada creyente crezca en su fe. ¡Es la hora del entrenamiento espiritual!",
+      "El camino hacia la madurez es un camino con desafíos de disciplina.",
+    ],
+  },
+  {
+    titulo: "Ministerio",
+    parrafos: [
+      "En cada clase serás motivado a desarrollar un corazón que sirve a Dios. ¿Qué enseña la Biblia acerca del ministerio? ",
+      "Recibirás orientación acerca del proceso de descubrir tu lugar único en el cuerpo: tu ministerio.<br>Te ayudaremos a descubrir los dones espirituales, talentos naturales y recursos que tiene y puedes desarrollar para servir al Señor.",
+    ],
+  },
+  {
+    titulo: "Liderazgo",
+    parrafos: [
+      "Serás instruido e instrumentado con herramientas prácticas para direccionar equipos de personas, abordando situaciones problemáticas y con la ayuda del Espíritu Santo traer luz y sanidad.",
+    ],
+  },
+];
