10a9ff40d3a3081206c3d223e412fed655d0cab7
Joel <joeldario9912@gmail.com>
2024-07-06 12:44:03 -0300

    estilos escuela crecimiento

--- src/libs/faqs.ts
+++ src/libs/faqs.ts
@@ -2,10 +2,10 @@ export const faqs : Faq[]=
 [
   {
     pregunta: "¿Qué es un Grupo Pequeño?",
-    respuesta: "Es un grupo de 5 a 10 personas que nos juntamos a orar y comer."
+    respuesta: "Es un grupo de 5 a 10 personas que nos juntamos a orar por nuestras necesidades y hacer amigos."
   },
   {
-    pregunta: "¿Qué es la Escuela de Crecimiento?",
-    respuesta: " Es un lugar para aprender mas de la Biblia."
+    pregunta: "¿Quién puede asistir?",
+    respuesta: "Toda persona que quiera buscar y encontrar a Dios."
   }
 ]
--- src/libs/ministerios.ts
+++ src/libs/ministerios.ts
@@ -3,54 +3,54 @@ export const ministerios: Ministerio[] = [
     icono: "finanzas",
     titulo: "finanzas",
     descripcion:
-      "Este ministerio tiene como objetivo administrar los recursos materiales, financieros y económicos que ingresan y egresan en la congregación local. Realizando presupuestos, balances, proyecciones, estadísticas contables, etc. A su vez, este ministerio interactúa con distintas áreas de la iglesia y con todos los ministerios, diseñando un presupuesto mensual y anual para cada caso, según sus prioridades y requerimientos evaluados previamente.",
+      "Tiene como objetivo administrar los recursos materiales, financieros y económicos que ingresan y egresan en la congregación local. Realizando presupuestos, balances, proyecciones, estadísticas contables, etc. A su vez, interactúa con distintas áreas de la iglesia y con todos los ministerios, diseñando un presupuesto mensual y anual para cada caso, según sus prioridades y requerimientos evaluados previamente.",
   },
   {
     icono: "jovenes",
     titulo: "jóvenes",
     descripcion:
-      "El grupo está dirigido a jóvenes de 11 a 17 años. El deseo de Dios y de nuestra Iglesia es que seas parte de una generación apasionada por Dios. Nos reunimos todos los domingos al mismo horario de la reunión familiar, acompáñanos y juntos celebremos al Señor.",
+      "El grupo está dirigido a jóvenes de 11 a 17 años. El deseo es que seas parte de una generación apasionada por Dios. Nos reunimos todos los sábados en \"El Palco\" para compartir un momento especial.",
   },
-  {
-    icono: "mantenimiento",
-    titulo: "mantenimiento",
-    descripcion:
-      "Este ministerio es muy específico, ya que las tareas que se llevan a cabo deben ser ejecutadas por personas idóneas y profesionales en la materia. Una de las características diferenciales de este ministerio, es que no es ejecutado constantemente y la mayoría de las veces se lleva a cabo cuando no hay actividad en las instalaciones o lugar de reunión.",
-  },
+  // {
+  //   icono: "mantenimiento",
+  //   titulo: "mantenimiento",
+  //   descripcion:
+  //     "Este ministerio es muy específico, ya que las tareas que se llevan a cabo deben ser ejecutadas por personas idóneas y profesionales en la materia. Una de las características diferenciales de este ministerio, es que no es ejecutado constantemente y la mayoría de las veces se lleva a cabo cuando no hay actividad en las instalaciones o lugar de reunión.",
+  // },
   {
     icono: "multimedia",
     titulo: "multimedia",
     descripcion:
-      "En este ministerio se realizan tareas relacionadas con el manejo de los medios audiovisuales que la Iglesia local utiliza en cada servicio o evento especial que se lleve a cabo. Operando distintas herramientas, notebook, proyector, software, unidades externas USB, conectores, etc. A su vez, este ministerio interactúa con otros, como ser: sonido, alabanza y adoración, exposición de la palabra, etc.",
+      "Realizan tareas relacionadas con el manejo de los medios audiovisuales que la Iglesia local utiliza en cada servicio o evento especial. Operando distintas herramientas, notebook, proyector, software, unidades externas USB, conectores, etc. A su vez, este ministerio interactúa con otros, como ser: sonido, alabanza y adoración, exposición de la palabra, etc.",
   },
   {
     icono: "secretaria",
     titulo: "secretaria",
     descripcion:
-      "En este ministerio se realizan tareas de administración, gestión y comunicación de la Iglesia, se imprimen materiales, folletos, libros, se realizan banners de comunicación digital y tareas en común con el ministerio de multimedia. Las personas que llevan adelante este ministerio también son responsables del mantenimiento digital de la página web y de todas las redes sociales (Facebook, Instagram y YouTube) que actualmente poseen la Iglesia local.",
+      "Realizan tareas de administración, gestión y comunicación de la Iglesia, se imprimen materiales, folletos, libros, se realizan banners de comunicación digital y tareas en común con el ministerio de multimedia. Las personas que llevan adelante este ministerio también son responsables del mantenimiento digital de la página web y de todas las redes sociales (Facebook, Instagram y YouTube) que actualmente poseen la Iglesia local.",
   },
   {
     icono: "sonido",
     titulo: "sonido",
     descripcion:
-      "Este ministerio es muy específico, ya que las tareas que se llevan a cabo deben ser ejecutadas por personas idóneas y profesionales en la materia, en cada servicio o evento especial que se realice. En el mismo se operan consolas de sonido, luces, instrumentos, amplificadores, potencias, parlantes, etc. A su vez, este ministerio interactúa con otros, como ser: multimedia, alabanza y adoración, exposición de la palabra, decoración, eventos, etc.",
+      "Manipula consolas de sonido, luces, instrumentos, amplificadores, potencias, parlantes, etc. A su vez, este ministerio interactúa con otros, como ser: multimedia, alabanza y adoración, exposición de la palabra, decoración, eventos, etc.",
   },
   {
     icono: "ujieres",
     titulo: "ujieres",
     descripcion:
-      "Este ministerio cumple una función muy importante en la familia de Dios, las personas que forman parte del mismo, son las encargadas de recibir con amabilidad, cordialidad y buena disposición a todas las personas que ingresan y participan de una reunión, servicio o evento especial. Deben estar siempre a disposición para servir al prójimo y lograr que una visita se sienta contenida y cómoda, como si estuviera en su propio hogar.",
+      "Las personas que forman son las encargadas de recibir con amabilidad, cordialidad y buena disposición a todas las personas que ingresan y participan de una reunión, servicio o evento especial. Deben estar siempre a disposición para servir al prójimo y lograr que una visita se sienta contenida y cómoda, como si estuviera en su propio hogar.",
   },
   {
     icono: "alabanza",
     titulo: "alabanza",
     descripcion:
-      "Guiar al pueblo de Dios a tener un momento de intimidad con Él, atraer la presencia de su Santo Espíritu y proclamar sus virtudes.",
+      "Guia al pueblo de Dios en alabanza y a tener un momento de intimidad con Él, atraer la presencia de su Santo Espíritu y proclamar sus virtudes.",
   },
   {
     icono: "radio",
     titulo: "radio encuentro",
     descripcion:
-      "Es la radio de Sunchales y la región que lleva un mensaje diferente, no se trata de religión, el objetivo es llevar el mensaje de Jesús de una manera dinámica y moderna. Destacamos a Dios sobre todas las cosas, llevando fe, amor y perdón a través de la música, mensajes, reflexiones, pensamientos y nuestros programas en vivo.",
+      "Es la radio de Sunchales y la región que lleva un mensaje diferente. El objetivo es llevar el mensaje de Jesús de una manera dinámica y moderna. Destacamos a Dios sobre todas las cosas, llevando fe, amor y perdón a través de la música, mensajes, reflexiones, pensamientos y nuestros programas en vivo.",
   },
 ];
